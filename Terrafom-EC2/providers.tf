68f608fee17417a816b9bd71ee0f0f541af9785f
JonesKwmaeOsei <oseikwamejones@gmail.com>
2024-04-30 13:56:47 +0100

    updated provider block

--- Terrafom-EC2/providers.tf
+++ Terrafom-EC2/providers.tf
@@ -5,17 +5,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.60.0" # The tilde ensures that the version is compatible with 4.60.0
+      source = "hashicorp/aws"
+      version = "5.47.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
 }
-
-# provider "aws" {
-#   region = "us-east-1"
-#   alias  = "prod"
-# }
