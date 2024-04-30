87604310f224da201bb1986f49f195bf09c92eb9
JonesKwmaeOsei <oseikwamejones@gmail.com>
2024-04-30 11:09:33 +0100

    Updated config files

--- Terraform/Output.tf
+++ Terraform/Output.tf
@@ -1,35 +1,35 @@
-# Output configuration details
-// The output of this script is a JSON object containing the following information:
-// ----------------------------------
+# # Output configuration details
+# // The output of this script is a JSON object containing the following information:
+# // ----------------------------------
 
-output "vpc_id" {
-  value = aws_vpc.vpc_web.id
-}
+# output "vpc_id" {
+#   value = aws_vpc.vpc_web.id
+# }
 
-output "vpc_cidr_block" {
-  value = aws_vpc.vpc_web.cidr_block
-}
+# output "vpc_cidr_block" {
+#   value = aws_vpc.vpc_web.cidr_block
+# }
 
-# Output Public IP
-output "web_server_public_ip" {
-  value = aws_eip.elastic_web.public_ip
-}
+# # Output Public IP
+# output "web_server_public_ip" {
+#   value = aws_eip.elastic_web.public_ip
+# }
 
-output "public_ipv4_address" {
-  value = aws_instance.ec2_web.public_ip
-}
+# output "public_ipv4_address" {
+#   value = aws_instance.ec2_web.public_ip
+# }
 
-# Output Private IP
-output "web_server_private_ip" {
-  value = aws_instance.ec2_web.private_ip
-}
+# # Output Private IP
+# output "web_server_private_ip" {
+#   value = aws_instance.ec2_web.private_ip
+# }
 
-# Output Web Server ID
-output "web_server_id" {
-  value = aws_instance.ec2_web.id
-}
+# # Output Web Server ID
+# output "web_server_id" {
+#   value = aws_instance.ec2_web.id
+# }
 
-# Output AMI Image Name
-output "ami_name" {
-  value = data.aws_ami.ubuntu_latest
-}
+# # Output AMI Image Name
+# output "ami_name" {
+#   value = data.aws_ami.ubuntu_latest
+# }
